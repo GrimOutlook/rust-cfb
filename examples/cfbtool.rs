e58ae76f1ddeb5504475857d967145d6139e2ab1
Dominic Grimaldi <self@grimoutlook.dev>
2025-12-09 13:08:21 -0600

    feat: Add example to dump streams

--- examples/cfbtool.rs
+++ examples/cfbtool.rs
@@ -1,6 +1,7 @@
-use std::io;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+use std::{env, fs, io};
 
+use cfb::CompoundFile;
 use clap::{Parser, Subcommand};
 use time::OffsetDateTime;
 use uuid::Uuid;
@@ -32,6 +33,56 @@ enum Command {
 
         path: Vec<String>,
     },
+
+    /// Dump a given stream by navigating to it from Root Storage.
+    Dump {
+        #[clap(short, long)]
+        /// Dump all streams found in CFB file.
+        all: bool,
+        /// Path to dump destination
+        path: String,
+    },
+}
+
+const TABLE_PREFIX: char = '\u{4840}';
+fn from_b64(value: u32) -> char {
+    debug_assert!(value < 64);
+    if value < 10 {
+        char::from_u32(value + '0' as u32).unwrap()
+    } else if value < 36 {
+        char::from_u32(value - 10 + 'A' as u32).unwrap()
+    } else if value < 62 {
+        char::from_u32(value - 36 + 'a' as u32).unwrap()
+    } else if value == 62 {
+        '.'
+    } else {
+        '_'
+    }
+}
+
+/// Decodes a stream name, and returns the decoded name and whether the stream
+/// was a table.
+fn decode(name: &str) -> (String, bool) {
+    let mut output = String::new();
+    let mut is_table = false;
+    let mut chars = name.chars().peekable();
+    if chars.peek() == Some(&TABLE_PREFIX) {
+        is_table = true;
+        chars.next();
+    }
+    for chr in chars {
+        let value = chr as u32;
+        if (0x3800..0x4800).contains(&value) {
+            let value = value - 0x3800;
+            output.push(from_b64(value & 0x3f));
+            output.push(from_b64(value >> 6));
+        } else if (0x4800..0x4840).contains(&value) {
+            output.push(from_b64(value - 0x4800));
+        } else {
+            output.push(chr);
+        }
+    }
+    (output, is_table)
 }
 
 fn split(path: &str) -> (PathBuf, PathBuf) {
@@ -116,5 +167,117 @@ fn main() {
                 }
             }
         }
+        Command::Dump { path, all } => {
+            let mut comp = cfb::open(&path).unwrap();
+            let mut entries = comp.read_root_storage().collect::<Vec<_>>();
+            if all {
+                let output_dir = env::current_dir().unwrap().join("root");
+                fs::create_dir(&output_dir).unwrap();
+                let root_entry = &comp.root_entry();
+                dump_entry_recursively(&mut comp, root_entry, &output_dir);
+                return;
+            }
+
+            loop {
+                for (index, subentry) in
+                    entries.clone().into_iter().enumerate()
+                {
+                    let (name, _) = decode(subentry.name());
+                    println!("[{index}] {}", name);
+                }
+                println!("Inspect?: ");
+                let mut input = String::new();
+                io::stdin()
+                    .read_line(&mut input)
+                    .expect("Failed to read line");
+                let input = input.trim();
+                if input == "q" {
+                    return;
+                }
+
+                let selected_index: usize = input
+                    .parse()
+                    .expect("Selection was not a valid number or 'q'");
+                let selection = entries
+                    .get(selected_index)
+                    .expect("Selected index was invalid");
+
+                if selection.is_storage() {
+                    entries = comp
+                        .read_storage(selection.name())
+                        .expect("Failed to read storage for selection")
+                        .collect();
+                } else if selection.is_stream() {
+                    let mut stream = comp
+                        .open_stream(selection.name())
+                        .expect("Failed to read stream for selection");
+                    println!("Stream dump location: ");
+                    let mut input = String::new();
+                    io::stdin()
+                        .read_line(&mut input)
+                        .expect("Failed to read line");
+                    let input = input.trim();
+                    println!(
+                        "Dumping stream [{}] to [{}]",
+                        selection.name(),
+                        input
+                    );
+                    let mut new_file = std::fs::File::options()
+                        .read(true)
+                        .write(true)
+                        .create(true)
+                        .truncate(true)
+                        .create_new(true)
+                        .open(input)
+                        .expect("Failed to create new file");
+
+                    std::io::copy(&mut stream, &mut new_file)
+                        .expect("Failed to copy data from stream");
+                    return;
+                }
+            }
+        }
     }
+}
+
+fn dump_entry_recursively<T: std::io::Seek + std::io::Read>(
+    comp: &mut CompoundFile<T>,
+    entry: &cfb::Entry,
+    output_dir: &Path,
+) {
+    if entry.is_root() || entry.is_storage() {
+        let entries = if entry.is_root() {
+            comp.read_root_storage().collect::<Vec<cfb::Entry>>()
+        } else {
+            comp.read_storage(entry.name())
+                .unwrap()
+                .collect::<Vec<cfb::Entry>>()
+        };
+
+        for subentry in entries {
+            let output_dir = output_dir.join(decode(subentry.name()).0);
+            fs::create_dir(output_dir.clone()).unwrap();
+            dump_entry_recursively(comp, &subentry, &output_dir);
+        }
+        return;
+    }
+
+    let mut stream = comp
+        .open_stream(entry.name())
+        .expect("Failed to read stream for selection");
+    let name = decode(entry.name()).0;
+    let output_location = output_dir.join(format!("{}.dump", name));
+
+    println!("Dumping stream [{}] to [{:#?}]", name, output_location);
+    let mut new_file = std::fs::File::options()
+        .read(true)
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .create_new(true)
+        .open(output_location)
+        .expect("Failed to create new file");
+
+    std::io::copy(&mut stream, &mut new_file)
+        .expect("Failed to copy data from stream");
 }
